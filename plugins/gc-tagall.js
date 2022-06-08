aa290afcca76c097e7d20904500ce6b6011d2526
Pedro Rivera <78751970+AckorEXE@users.noreply.github.com>
2022-06-07 23:34:25 -0500

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -4,8 +4,8 @@ global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = `🔔 ${pesan}`
-let teks = `*💢𝘔𝘈𝘚𝘚 𝘗𝘖𝘒𝘌💢*\n${oi}\n\n🧙‍♂️ *𝘗𝘓𝘈𝘠𝘌𝘙𝘚:*\n`
+let oi = `🔔${pesan}`
+let teks = `*💢𝘔𝘈𝘚𝘚 𝘗𝘖𝘒𝘌💢*\n${oi}\n\n🧙‍♂️*𝘗𝘓𝘈𝘠𝘌𝘙𝘚:*\n`
 for (let mem of participants) {
 teks += `┣➥ @${mem.id.split('@')[0]}\n`}
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
