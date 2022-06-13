f7659b538425dab4982d63d8665d71444a4405ef
Pedro Rivera <78751970+AckorEXE@users.noreply.github.com>
2022-06-13 18:27:35 -0500

    Update gc-config.js

--- plugins/gc-config.js
+++ plugins/gc-config.js
@@ -7,10 +7,9 @@ let isClose = { // Switch Case Like :v
 }[(args[0] || '')]
 if (isClose === undefined)
 throw `
-*[❗] 𝙵𝙾𝚁𝙼𝙰𝚃𝙾 𝙴𝚁𝚁𝙾𝙽𝙴𝙾!!*
-*┏━━━❲ ✨𝙴𝙹𝙴𝙼𝙿𝙻𝙾✨ ❳━━━┓* 
-*┠┉↯ ${usedPrefix + command} abrir*
-*┠┉↯ ${usedPrefix + command} cerrar*
+*[❗] 𝘍𝘖𝘙𝘔𝘈𝘛𝘖 𝘌𝘙𝘙𝘖𝘕𝘌𝘖*
+*Ejemplo:* 
+*${usedPrefix + command} open / close*
 `.trim()
 await conn.groupSettingUpdate(m.chat, isClose)
 {m.reply('*[✔] 𝘎𝘙𝘜𝘗𝘖 𝘊𝘖𝘕𝘍𝘐𝘎𝘜𝘙𝘈𝘋𝘖 𝘊𝘖𝘙𝘙𝘌𝘊𝘛𝘈𝘔𝘌𝘕𝘛𝘌.*')}
