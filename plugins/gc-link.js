aad23a6a4986ef6913880515dc9dce5f662b3cdc
Pedro Rivera <78751970+AckorEXE@users.noreply.github.com>
2022-06-13 18:22:41 -0500

    Update gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
 let group = m.chat
 conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
-title: '* 𝘓𝘐𝘕𝘒 𝘋𝘌𝘓 𝘎𝘙𝘜𝘗𝘖 *',
+title: '𝘓𝘐𝘕𝘒 𝘋𝘌𝘓 𝘎𝘙𝘜𝘗𝘖',
 body: '🤖𝕏𝕜ℝ-𝔹𝕠𝕥🤖 ',        
 previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
 sourceUrl: ``}}})   
