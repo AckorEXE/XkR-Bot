7a0d43f3197fb6677317e11e65c92e750b007b18
Pedro Rivera <78751970+AckorEXE@users.noreply.github.com>
2022-06-14 00:06:03 -0500

    Update rashid.js

--- plugins/rashid.js
+++ plugins/rashid.js
@@ -8,6 +8,12 @@ throw `
 Hoy esta en venore.
 `.trim()
   
+let respuesta =`
+(dia)
+`.trim()
+
+conn.sendHydrated(m.chat, respuesta, [], m)}
+
 handler.help = ['rashid']
 handler.tags = ['tools']
 handler.command = /^(rashid)$/i
