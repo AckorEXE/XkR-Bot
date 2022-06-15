422b4fe7f08bd2cab91dd51f2166c57f757a6b8f
Pedro Rivera <78751970+AckorEXE@users.noreply.github.com>
2022-06-15 15:39:32 -0500

    Update rashid.js

--- plugins/rashid.js
+++ plugins/rashid.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 let handler = async (m, { conn, args }) => {
     
-const days = [
+const day = [
     {"day": "Domingo", "local": "Carlin"},
     {"day":"Lunes", "local":"Svargrond"},
     {"day":"Martes", "local":"Liberty Bay"},
@@ -16,7 +16,7 @@ console.log(date.getUTCDay())
 
 days.forEach((e, i) => {
     if(i == date.getUTCDay()){
-        console.log(`Rashid at ${e.local} - ${e.day}`)
+        conn.reply(`Rashid at ${e.local} - ${e.day}`)
     }
 })
   
