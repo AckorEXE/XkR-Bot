2758f1f7f1bc863ae67c69fd682e2cce80dca871
Pedro Rivera <78751970+AckorEXE@users.noreply.github.com>
2022-10-31 16:04:01 -0600

    Update menu2.js

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -22,7 +22,7 @@ let estado =`
 ╰──────────
 `.trim()
 
-conn.sendHydrated(m.chat, estado, wm, picture, 'https://wa.me/528251002140/?text=Hola%20me%20interesa%20t%C3%BA%20servicio%20de%20bot.', 'CONTACTO', null, null, [], m)}
+conn.sendHydrated(m.chat, estado, wm, picture, m)}
 
 handler.help = ['menu']
 handler.tags = ['main']
