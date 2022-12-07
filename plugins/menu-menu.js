bfd6c8eeae37c115d51b3d3be6d44cf14d04850e
Pedro Rivera <78751970+AckorEXE@users.noreply.github.com>
2022-12-06 19:16:47 -0600

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -1,22 +1,28 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
-
-
+let picture = './Menu1.jpg'
+let name = await conn.getName(m.sender)
+let _uptime = process.uptime() * 1000
+let _muptime
+if (process.send) { process.send('uptime')
+_muptime = await new Promise(resolve => { process.once('message', resolve) 
+setTimeout(resolve, 1000) }) * 1000}
+let uptime = clockString(_uptime)
 let estado =`
-╭─[ *:robot:𝕏𝕜ℝ-𝔹𝕠𝕥:robot:* ]
-│ 
+╭─[ *🤖𝕏𝕜ℝ-𝔹𝕠𝕥🤖* ]
+│ *👋🏻 Hola ${name},*
 |
-│ :star: *𝘔𝘌𝘕𝘜 𝘋𝘌𝘓 𝘉𝘖𝘛:*
-│ :gem: #mp _*<texto>*_
-│ :gem: #add _*<número>*_
-│ :gem: #kick _*<número>*_
-│ :gem: #gc _*<open / close>*_
-│ :gem: #link
-│ :gem: #estado
-│ :gem: #tts _*<texto>*_
+│ ⭐ *𝘔𝘌𝘕𝘜 𝘋𝘌𝘓 𝘉𝘖𝘛:*
+│ 💎 #mp _*<texto>*_
+│ 💎 #add _*<número>*_
+│ 💎 #kick _*<número>*_
+│ 💎 #gc _*<open / close>*_
+│ 💎 #link
+│ 💎 #estado
+│ 💎 #tts _*<texto>*_
 ╰──────────
 `.trim()
 
-conn.sendHydrated(m.chat, estado, m)}
+conn.sendHydrated(m.chat, estado, wm, picture, 'https://wa.me/528251002140/?text=Hola%20me%20interesa%20t%C3%BA%20servicio%20de%20bot.', 'CONTACTO', null, null, [], m)}
 
 handler.help = ['menu']
 handler.tags = ['main']
@@ -24,3 +30,8 @@ handler.command = /^(menu)$/i
 export default handler
 
 
+function clockString(ms) {
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
