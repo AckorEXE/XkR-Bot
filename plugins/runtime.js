3deae83fd4773f11d46dfe3dd144ee55e715e95f
Pedro Rivera <78751970+AckorEXE@users.noreply.github.com>
2022-12-06 20:42:45 -0600

    Update runtime.js

--- plugins/runtime.js
+++ plugins/runtime.js
@@ -2,12 +2,13 @@ let handler = async (m, { conn, command, usedPrefix }) => {
 
 m.reply(`
 *┌─〔 ＡＣＴＩＶＯ 〕*
-*├ 𝚃𝙸𝙴𝙼𝙿𝙾 𝙰𝙲𝚃𝙸𝚅𝙾: *
+*├ okey
 *└───────────────*
 `.trim())}
 
-handler.help = ['runtime']
-handler.tags = ['info']
-handler.command = /^(uptime|runtime)$/i
-module.exports = handler
+
+handler.help = ['menu']
+handler.tags = ['main']
+handler.command = /^(info)$/i
+export default handler
 
