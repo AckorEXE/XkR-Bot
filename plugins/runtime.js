bf0863daad7d28b30d48309bc672b78e19cb473f
Pedro Rivera <78751970+AckorEXE@users.noreply.github.com>
2022-12-06 20:43:52 -0600

    Update runtime.js

--- plugins/runtime.js
+++ plugins/runtime.js
@@ -1,8 +1,10 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
+let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime)
 
 m.reply(`
 *┌─〔 ＡＣＴＩＶＯ 〕*
-*├ okey
+*├ 𝚃𝙸𝙴𝙼𝙿𝙾 𝙰𝙲𝚃𝙸𝚅𝙾: ${uptime}*
 *└───────────────*
 `.trim())}
 
@@ -12,3 +14,8 @@ handler.tags = ['main']
 handler.command = /^(info)$/i
 export default handler
 
+function clockString(ms) {
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
