bc50214d8804f0391b59debd6fb84fcad4342874
Pedro Rivera <78751970+AckorEXE@users.noreply.github.com>
2022-12-06 19:50:13 -0600

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -1,12 +1,5 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
-let picture = './Menu1.jpg'
-let name = await conn.getName(m.sender)
-let _uptime = process.uptime() * 1000
-let _muptime
-if (process.send) { process.send('uptime')
-_muptime = await new Promise(resolve => { process.once('message', resolve) 
-setTimeout(resolve, 1000) }) * 1000}
-let uptime = clockString(_uptime)
+
 let estado =`
 ╭─[ *🤖𝕏𝕜ℝ-𝔹𝕠𝕥🤖* ]
 │ *👋🏻 Hola ${name},*
@@ -25,11 +18,4 @@ let estado =`
 handler.help = ['menu']
 handler.tags = ['main']
 handler.command = /^(menu)$/i
-export default handler
 
-
-function clockString(ms) {
-let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
