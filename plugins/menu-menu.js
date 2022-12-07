2453565893a7274bf670a24e2d8c6e4088cba6d1
Pedro Rivera <78751970+AckorEXE@users.noreply.github.com>
2022-12-06 19:48:42 -0600

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -1,74 +1,37 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
-let menu = `
-╭══〘 ✯✯✯✯✯✯✯✯ 〙═╮
-║≡≡≡≡≡≡≡≡≡≡≡≡≡≡
-║➤ *✨𝗛ola, ${taguser} :D*
-║≡≡≡≡≡≡≡≡≡≡≡≡≡≡
-╰══╡✯✯✯✯✯✯✯✯╞══╯
-┏━━━━━━━━━━━━━┓
-┃ *< COMANDOS >*
-┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡
-┣ ඬ⃟💫 _a_
-┣ ඬ⃟💫 _${usedPrefix}cat_
-┣ ඬ⃟💫 _${usedPrefix}dog_
-┣ ඬ⃟💫 _${usedPrefix}meme_
-┣ ඬ⃟💫 _${usedPrefix}logos_
-┣ ඬ⃟💫 _${usedPrefix}runtime_
-┣ ඬ⃟💫 _${usedPrefix}infohost_
-┣ ඬ⃟💫 _${usedPrefix}on welcome_
-┣ ඬ⃟💫 _${usedPrefix}off welcome_
-┣ ඬ⃟💫 _${usedPrefix}mediafire *[url]*_
-┣ ඬ⃟💫 _${usedPrefix}tiktok *[url]*_
-┣ ඬ⃟💫 _${usedPrefix}sticker *[url]*_
-┣ ඬ⃟💫 _${usedPrefix}attp *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}attp2 *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}attp3 *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}ttp *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}ttp2 *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}ttp3 *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}ttp4 *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}ttp5 *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}wikipedia *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}google *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}imagen *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}play *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}play2 *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}play3 *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}invocar *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}ytsearch *[texto]*_
-┣ ඬ⃟💫 _${usedPrefix}getaud *[url]*_
-┣ ඬ⃟💫 _${usedPrefix}getvid *[url]*_
-┣ ඬ⃟💫 _${usedPrefix}ytmp3 *[url]*_
-┣ ඬ⃟💫 _${usedPrefix}ytmp4 *[url]*_
-┣ ඬ⃟💫 _${usedPrefix}tts *[lenguaje] [texto]*_
-┣ ඬ⃟💫 _${usedPrefix}toimg *[sticker]*_
-┣ ඬ⃟💫 _${usedPrefix}sticker *[imagen]*_
-┣ ඬ⃟💫 _${usedPrefix}tourl *[imagen]*_
-┣ ඬ⃟💫 _${usedPrefix}tourl *[video]*_
-┣ ඬ⃟💫 _${usedPrefix}tourl *[audio]*_
-┣ ඬ⃟💫 _${usedPrefix}fat *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}bass *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}blown *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}deep *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}fast *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}robot *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}slow *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}tupai *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}vibra *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}nightcore *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}earrape *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}reverse *[nota de voz]*_
-┣ ඬ⃟💫 _${usedPrefix}smooth *[nota de voz]*_
-┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡
-┃ *< OWNER >*
-┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡
-┣ ඬ⃟💫 _${usedPrefix}update_
-┣ ඬ⃟💫 _${usedPrefix}banchat_
-┣ ඬ⃟💫 _${usedPrefix}unbanchat_
-┗━━━━━━━━━━━━━┛
+let picture = './Menu1.jpg'
+let name = await conn.getName(m.sender)
+let _uptime = process.uptime() * 1000
+let _muptime
+if (process.send) { process.send('uptime')
+_muptime = await new Promise(resolve => { process.once('message', resolve) 
+setTimeout(resolve, 1000) }) * 1000}
+let uptime = clockString(_uptime)
+let estado =`
+╭─[ *🤖𝕏𝕜ℝ-𝔹𝕠𝕥🤖* ]
+│ *👋🏻 Hola ${name},*
+|
+│ ⭐ *𝘔𝘌𝘕𝘜 𝘋𝘌𝘓 𝘉𝘖𝘛:*
+│ 💎 #mp _*<texto>*_
+│ 💎 #add _*<número>*_
+│ 💎 #kick _*<número>*_
+│ 💎 #gc _*<open / close>*_
+│ 💎 #link
+│ 💎 #estado
+│ 💎 #tts _*<texto>*_
+╰──────────
 `.trim()
+
+conn.sendHydrated(m.chat, estado, wm, picture, 'https://wa.me/528251002140/?text=Hola%20me%20interesa%20t%C3%BA%20servicio%20de%20bot.', 'CONTACTO', null, null, [], m)}
 
 handler.help = ['menu']
 handler.tags = ['main']
 handler.command = /^(menu)$/i
 export default handler
+
+
+function clockString(ms) {
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
