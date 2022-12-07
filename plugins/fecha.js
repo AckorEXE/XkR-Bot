83c7a0b76de1ed647fb3eccc72c9214265801a64
Pedro Rivera <78751970+AckorEXE@users.noreply.github.com>
2022-12-07 02:23:58 -0600

    Create fecha.js

---
+++ plugins/fecha.js
@@ -0,0 +1,14 @@
+let handler = async (m, { conn, command, usedPrefix }) => {
+
+const date = new Date(date.UTC);
+const currentDay = date.getDay();
+ 
+  
+m.reply((date.toLocaleString());
+}
+
+handler.help = ['fecha']
+handler.tags = ['main']
+handler.command = /^(fecha)$/i
+handler.group = true
+export default handler
