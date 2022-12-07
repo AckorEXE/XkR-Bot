9ed8f9a5910f840ae65742ba6c61ef3359074743
Pedro Rivera <78751970+AckorEXE@users.noreply.github.com>
2022-12-07 00:18:08 -0600

    Update convertidor-tts.js

--- plugins/convertidor-tts.js
+++ plugins/convertidor-tts.js
@@ -26,6 +26,7 @@ if (res) conn.sendFile(m.chat, res, 'tts.opus', null, m, true)
 handler.help = ['tts <lang> <teks>']
 handler.tags = ['tools']
 handler.command = /^g?tts$/i
+handler.group = true
 export default handler
 
 function tts(text, lang = 'es') {
