e8c391b9aeeed6289995e1ecef211ac6cff2164c
Pedro Rivera <78751970+AckorEXE@users.noreply.github.com>
2022-12-06 19:51:53 -0600

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -1,21 +1,101 @@
-let handler = async (m, { conn, command, usedPrefix }) => {
-
-let estado =`
-╭─[ *🤖𝕏𝕜ℝ-𝔹𝕠𝕥🤖* ]
-│ *👋🏻 Hola ${name},*
-|
-│ ⭐ *𝘔𝘌𝘕𝘜 𝘋𝘌𝘓 𝘉𝘖𝘛:*
-│ 💎 #mp _*<texto>*_
-│ 💎 #add _*<número>*_
-│ 💎 #kick _*<número>*_
-│ 💎 #gc _*<open / close>*_
-│ 💎 #link
-│ 💎 #estado
-│ 💎 #tts _*<texto>*_
-╰──────────
+let fs = require('fs')
+let handler = async (m, { conn, usedPrefix }) => {
+let pp = './Menu2.jpg'
+let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+let menu = `
+╭══〘 ✯✯✯✯✯✯✯✯ 〙═╮
+║≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+║➤ *✨𝗛ola, ${taguser} :D*
+║≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+╰══╡✯✯✯✯✯✯✯✯╞══╯
+┏━━━━━━━━━━━━━┓
+┃ *< COMANDOS >*
+┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+┣ ඬ⃟💫 _a_
+┣ ඬ⃟💫 _${usedPrefix}cat_
+┣ ඬ⃟💫 _${usedPrefix}dog_
+┣ ඬ⃟💫 _${usedPrefix}meme_
+┣ ඬ⃟💫 _${usedPrefix}logos_
+┣ ඬ⃟💫 _${usedPrefix}runtime_
+┣ ඬ⃟💫 _${usedPrefix}infohost_
+┣ ඬ⃟💫 _${usedPrefix}on welcome_
+┣ ඬ⃟💫 _${usedPrefix}off welcome_
+┣ ඬ⃟💫 _${usedPrefix}mediafire *[url]*_
+┣ ඬ⃟💫 _${usedPrefix}tiktok *[url]*_
+┣ ඬ⃟💫 _${usedPrefix}sticker *[url]*_
+┣ ඬ⃟💫 _${usedPrefix}attp *[texto]*_
+┣ ඬ⃟💫 _${usedPrefix}attp2 *[texto]*_
+┣ ඬ⃟💫 _${usedPrefix}attp3 *[texto]*_
+┣ ඬ⃟💫 _${usedPrefix}ttp *[texto]*_
+┣ ඬ⃟💫 _${usedPrefix}ttp2 *[texto]*_
+┣ ඬ⃟💫 _${usedPrefix}ttp3 *[texto]*_
+┣ ඬ⃟💫 _${usedPrefix}ttp4 *[texto]*_
+┣ ඬ⃟💫 _${usedPrefix}ttp5 *[texto]*_
+┣ ඬ⃟💫 _${usedPrefix}wikipedia *[texto]*_
+┣ ඬ⃟💫 _${usedPrefix}google *[texto]*_
+┣ ඬ⃟💫 _${usedPrefix}imagen *[texto]*_
+┣ ඬ⃟💫 _${usedPrefix}play *[texto]*_
+┣ ඬ⃟💫 _${usedPrefix}play2 *[texto]*_
+┣ ඬ⃟💫 _${usedPrefix}play3 *[texto]*_
+┣ ඬ⃟💫 _${usedPrefix}invocar *[texto]*_
+┣ ඬ⃟💫 _${usedPrefix}ytsearch *[texto]*_
+┣ ඬ⃟💫 _${usedPrefix}getaud *[url]*_
+┣ ඬ⃟💫 _${usedPrefix}getvid *[url]*_
+┣ ඬ⃟💫 _${usedPrefix}ytmp3 *[url]*_
+┣ ඬ⃟💫 _${usedPrefix}ytmp4 *[url]*_
+┣ ඬ⃟💫 _${usedPrefix}tts *[lenguaje] [texto]*_
+┣ ඬ⃟💫 _${usedPrefix}toimg *[sticker]*_
+┣ ඬ⃟💫 _${usedPrefix}sticker *[imagen]*_
+┣ ඬ⃟💫 _${usedPrefix}tourl *[imagen]*_
+┣ ඬ⃟💫 _${usedPrefix}tourl *[video]*_
+┣ ඬ⃟💫 _${usedPrefix}tourl *[audio]*_
+┣ ඬ⃟💫 _${usedPrefix}fat *[nota de voz]*_
+┣ ඬ⃟💫 _${usedPrefix}bass *[nota de voz]*_
+┣ ඬ⃟💫 _${usedPrefix}blown *[nota de voz]*_
+┣ ඬ⃟💫 _${usedPrefix}deep *[nota de voz]*_
+┣ ඬ⃟💫 _${usedPrefix}fast *[nota de voz]*_
+┣ ඬ⃟💫 _${usedPrefix}robot *[nota de voz]*_
+┣ ඬ⃟💫 _${usedPrefix}slow *[nota de voz]*_
+┣ ඬ⃟💫 _${usedPrefix}tupai *[nota de voz]*_
+┣ ඬ⃟💫 _${usedPrefix}vibra *[nota de voz]*_
+┣ ඬ⃟💫 _${usedPrefix}nightcore *[nota de voz]*_
+┣ ඬ⃟💫 _${usedPrefix}earrape *[nota de voz]*_
+┣ ඬ⃟💫 _${usedPrefix}reverse *[nota de voz]*_
+┣ ඬ⃟💫 _${usedPrefix}smooth *[nota de voz]*_
+┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+┃ *< OWNER >*
+┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+┣ ඬ⃟💫 _${usedPrefix}update_
+┣ ඬ⃟💫 _${usedPrefix}banchat_
+┣ ඬ⃟💫 _${usedPrefix}unbanchat_
+┗━━━━━━━━━━━━━┛
 `.trim()
-
-handler.help = ['menu']
-handler.tags = ['main']
-handler.command = /^(menu)$/i
-
+let buttons = [
+{ buttonId: '#owner', buttonText: { displayText: '💫 𝐎𝐖𝐍𝐄𝐑 💫' }, type: 1 },
+{ buttonId: '#runtime', buttonText: { displayText: '⏰ 𝐑𝐔𝐍𝐓𝐈𝐌𝐄 ⏰' }, type: 1 },
+{ buttonId: '#infohost', buttonText: { displayText: '👑 𝐈𝐍𝐅𝐎𝐇𝐎𝐒𝐓 👑' }, type: 1 }]
+let buttonMessage = {
+image: fs.readFileSync('./Menu2.jpg'),
+caption: menu.trim(),
+mentions: [m.sender],
+footer: `*${wm}*`,
+buttons: buttons,
+headerType: 4,
+contextInfo: {
+mentionedJid: [m.sender],
+externalAdReply: {
+showAdAttribution: true,
+mediaType: 'VIDEO',
+mediaUrl: null,
+title: '👑 𝐀𝐜𝐢𝐝𝐢𝐜𝐍𝐨𝐝𝐞𝐬 𝐇𝐨𝐬𝐭 👑',
+body: null,
+thumbnail: fs.readFileSync('./src/logo.png'),
+sourceUrl: `https://chat.whatsapp.com/F0fU7LSlBBcBm6ny5fVSuT`
+}}}
+conn.sendMessage(m.chat, buttonMessage, { quoted: m })
+}
+handler.help = ['menu', 'help', '?']
+handler.tags = ['general']
+handler.command = /^(menucompleto|comandos|allmenu|info|speed|estado|menú|menu|help|\?)$/i
+handler.fail = null
+module.exports = handler
