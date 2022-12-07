38082fe4ee24c805f55ed106275d271f97ba2e80
Pedro Rivera <78751970+AckorEXE@users.noreply.github.com>
2022-12-07 02:45:34 -0600

    Update fecha.js

--- plugins/fecha.js
+++ plugins/fecha.js
@@ -1,6 +1,7 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
 
 var currentDate = new Date();
+date.setTimezoneOffset(-480)
 
 // Use the toLocaleString() method to format the date and time
 // in a more human-readable way
