a97b0d81346a3af974774b9bc019c1a8d5723286
Pedro Rivera <78751970+AckorEXE@users.noreply.github.com>
2022-12-08 01:21:58 -0600

    Update hora.js

--- plugins/hora.js
+++ plugins/hora.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, command, usedPrefix }) => {
 let chicago_datetime_str = new Date().toLocaleString("en-US", { timeZone: "America/Chicago" });
     
 // Add three hours to the current time
-    date.setHours(date.getHours() - 3);
+    date.setHours(date.getHours() - 10);
 
 var currentDay = date.getDay();
     
