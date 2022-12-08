f9da1cc931cd2938cd5189163ca705e34a668ed4
Pedro Rivera <78751970+AckorEXE@users.noreply.github.com>
2022-12-08 01:40:47 -0600

    Update hora.js

--- plugins/hora.js
+++ plugins/hora.js
@@ -1,41 +1,46 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
 
-let date = new Date();
-date.setHours(date.getHours() - 11);
-let dayOfWeek = date.getDay();
+    // Obten la fecha actual
+    let date = new Date();
 
-// Get the name of the day of the week
-let dayName;
-switch (dayOfWeek) {
-  case 0:
-    dayName = "Sunday";
-    break;
-  case 1:
-    dayName = "Monday";
-    break;
-  case 2:
-    dayName = "Tuesday";
-    break;
-  case 3:
-    dayName = "Wednesday";
-    break;
-  case 4:
-    dayName = "Thursday";
-    break;
-  case 5:
-    dayName = "Friday";
-    break;
-  case 6:
-    dayName = "Saturday";
-    break;
+    // A la fecha actual restale 10 horas
+    date.setHours(date.getHours() - 10);
+
+    // Obten el dia de la semana basandote en la nueva hora
+    let dayOfWeek = date.getDay();
+
+    // Imprime el dia de la semana en un caso switch usando la nueva hora
+    let dayName;
+    switch (dayOfWeek) {
+        case 0:
+            dayName = "*Domingo*, lo puedes encontrar en el depot de Carlin, un piso arriba.";
+            break;
+        case 1:
+            dayName = "*Lunes*, lo puedes encontrar en Svargrond, en la taberna de Dankwart, al sur del templo.";
+            break;
+        case 2:
+            dayName = "*Martes*, lo puedes encontrar en Liberty Bay, en la taberna de Lyonel, oeste del depot.";
+            break;
+        case 3:
+            dayName = "*Miércoles*, lo puedes encontrar en Port Hope, en la taberna de Clyde, al norte del barco.";
+            break;
+        case 4:
+            dayName = "*Jueves*, lo puedes encontrar en Ankrahmun, en la taberna de Arito, sobre la oficina postal.";
+            break;
+        case 5:
+            dayName = "*Viernes*, lo puedes encontrar en Darashia, en la taberna de Miraia, al sur de las guildhalls.";
+            break;
+        case 6:
+            dayName = "*Sábado*, lo puedes encontrar en Edron, en la taberna de Mirabell, sobre el depot.";
+            break;
+    }
+
+    // Envia el dia 
+    m.reply(dayName);
 }
 
-
-m.reply(dayName);
-}
-    
-    handler.help = ['hora']
-    handler.tags = ['main']
-    handler.command = /^(hora)$/i
-    handler.group = true
-    export default handler
+handler.help = ['hora']
+handler.tags = ['main']
+handler.command = /^(hora)$/i
+handler.group = true
+export default handler
